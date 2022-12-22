2707c5cd9371b0667069e3da0939ff158b8cb5f1
DAN <life4789@gmail.com>
2022-12-22 22:55:14 +0900

    속도개선

--- 5622.kt
+++ 5622.kt
@@ -1,35 +1,24 @@
 fun main() {
-
-    val alphabetsInNumber = mapOf(
-        1 to "",            // n + 2초
-        2 to "ABC",
-        3 to "DEF",
-        4 to "GHI",
-        5 to "JKL",
-        6 to "MNO",
-        7 to "PQRS",
-        8 to "TUV",
-        9 to "WXYZ",
-        0 to "OPERATOR"
-    )
-
     val input = readln()
     val alphabets = input.map{ it }
 
     var sum = 0
     for (i:Int in 0 until alphabets.count()){
-        sum += getKey(alphabetsInNumber, alphabets[i])!!.toInt() + 1
-        //println("alphabet : ${alphabets[i]}, sum : ${sum} ")
+        sum += getTime(alphabets[i])
     }
 
     print(sum)
 }
 
-fun getKey(map: Map<Int, String>, target: Char): Int?{
-    for ((key, value) in map){
-        if(value.contains(target)){
-            return key;
-        }
+fun getTime(alphabet: Char): Int{
+    return when(alphabet){
+        in 'A'..'C' -> 3
+        in 'D' .. 'F' -> 4
+        in 'G' .. 'I' -> 5
+        in 'J' .. 'L' -> 6
+        in 'M' .. 'O' -> 7
+        in 'P' .. 'S' -> 8
+        in 'T' .. 'V' -> 9
+        else -> 10
     }
-    return null
 }
