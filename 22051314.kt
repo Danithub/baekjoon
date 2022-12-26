af9b675a5151cbfc2502b3f6c7d0270b1ebb5147
DAN <life4789@gmail.com>
2022-12-26 17:08:52 +0900

    solved(속도개선)

--- 22051314.kt
+++ 22051314.kt
@@ -1,20 +1,14 @@
 fun main() {
     var N = readln().toInt()
 
-    aaa(N)
+    var number = 2
+    while(N > 1){
+        if(N % number == 0){
+            println(number)
+            N /= number
+        } else{
+            number++
+        }
+    }
 }
 
-var number = 2
-fun aaa(n: Int){
-    if(n <= 1){
-        return
-    }
-    if(n % number != 0) {
-        number++
-        aaa(n)
-        return
-    }
-
-    println(number)
-    aaa(n/number)
-}
