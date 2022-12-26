a4ce677e886e4e8f80c63255b1e43b0191922951
DAN <life4789@gmail.com>
2022-12-27 00:00:27 +0900

    solved(속도개선)

--- 9020.kt
+++ 9020.kt
@@ -1,15 +1,3 @@
-class Candidate(min: Int, max: Int) {
-    var min = 0
-    var max = 0
-    var distance = 0
-
-    init {
-        this.min = min
-        this.max = max
-        this.distance = max - min
-    }
-}
-
 fun main() {
     val count = readln().toInt()
 
@@ -17,25 +5,20 @@ fun main() {
     for (i: Int in 0 until count){
         val even = readln().toInt()
 
-        val primeList = mutableListOf<Int>()
-        for (j: Int in 2 until even){
-            if(isPrime(j)) primeList.add(j)
-        }
-
-        var candidateList = mutableListOf<Candidate>()
-        for (m:Int in 0 until primeList.size){
-            if(primeList.contains(even - primeList[m]) && primeList[m] <= even - primeList[m]){
-                val can = Candidate(primeList[m], even - primeList[m])
-                //println("min : ${can.min}, max : ${can.max}")
-                candidateList.add(can)
+        // n/2을 기준으로 +1/-1을 하며 두 수가 소수이고 합이 n인 값을 찾는다.
+        var lowNumber = even / 2
+        var highNumber = even / 2
+        while (true){
+            if(isPrime(lowNumber) && isPrime(highNumber)){
+                if(lowNumber + highNumber == even){
+                    result += "$lowNumber $highNumber\n"
+                    break;
+                }
             }
+            lowNumber--
+            highNumber++
         }
-
-        candidateList = candidateList.sortedBy { it.distance }.toMutableList()
-
-        result += "${candidateList.first().min} ${candidateList.first().max}\n"
     }
-
     print(result)
 }
 
