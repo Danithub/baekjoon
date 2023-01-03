2e965a059f58d19a20697aba299c0e3241412d1b
DAN <life4789@gmail.com>
2023-01-03 17:18:43 +0900

    solved
    속도개선

--- 10815.kt
+++ 10815.kt
@@ -9,35 +9,19 @@ fun main() {
     val bw = BufferedWriter(OutputStreamWriter(System.out))
 
     val n = br.readLine().toInt()
-    val numberCards = br.readLine().split(" ").map { it.toInt() }.sorted()
+    val numberCards = br.readLine().split(" ").map { it.toInt() }.toMutableSet()
     val m = br.readLine().toInt()
     val checkCards = br.readLine().split(" ").map { it.toInt() }
 
     val sb = StringBuilder()
     for (i:Int in checkCards.indices){
-        sb.append("${binarySearch(numberCards, checkCards[i])} ")
+        if(numberCards.add(checkCards[i])){
+            sb.append("0 ")
+        } else{
+            sb.append("1 ")
+        }
     }
 
     bw.write(sb.toString())
     bw.flush()
-}
-
-fun binarySearch(arr:List<Int>, number: Int):Int {
-    var low = 0
-    var high = arr.size - 1
-    var mid = 0
-
-    while (low <= high){
-        mid = (low + high) / 2
-
-        if(number == arr[mid]){
-            return 1
-        } else if (number < arr[mid]){
-            high = mid - 1
-        } else {
-            low = mid + 1
-        }
-    }
-
-    return 0
 }
