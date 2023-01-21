e4e74e8c29cba36fb59324b98964544acca2a6bd
DAN <life4789@gmail.com>
2023-01-21 18:49:41 +0900

    solved

---
+++ 1008.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    val (a, b) = br.readLine().split(" ").map { it.toDouble() }
+    bw.write("${(a/b)}")
+    bw.flush()
+}
