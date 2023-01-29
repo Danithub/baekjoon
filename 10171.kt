05f98bea9fc7170ec1ac552c61c7156a528974a7
DAN <life4789@gmail.com>
2023-01-29 17:50:13 +0900

    solved

---
+++ 10171.kt
@@ -0,0 +1,9 @@
+fun main(){
+    //val br = BufferedReader(InputStreamReader(System.`in`))
+    //val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+    println("\\    /\\")
+    println(" )  ( ')")
+    println("(  /  )")
+    println(" \\(__)|")
+}
